12b1044174bfc7907a810b1f9b9e64f8dda9c47a
Giovanni Aguirre <giovanni.fi05@gmail.com>
2023-11-10 01:02:45 -0600

    Initial REST API definition

---
+++ scripts/oas_codegen.sh
@@ -0,0 +1,11 @@
+# Current path
+# ref: https://stackoverflow.com/a/4774063/3211029
+HERE="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+
+docker run --rm                                         \
+    -v "${HERE}/codegen":/mnt/codegen                   \
+    -v "${HERE}/../docs/openapi.yaml:/mnt/openapi.yaml" \
+    openapitools/openapi-generator-cli generate         \
+      -i /mnt/openapi.yaml                              \
+      -g python                                         \
+      -o /mnt/codegen/
