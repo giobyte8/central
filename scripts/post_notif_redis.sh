e377bb66b6ee63c5f82e05dc9cff66654a00e7e2
Giovanni Aguirre <giovanni.fi05@gmail.com>
2024-02-04 22:20:20 -0800

    Add logging

--- scripts/post_notif_redis.sh
+++ scripts/post_notif_redis.sh
@@ -7,16 +7,23 @@
 # ref: https://stackoverflow.com/a/4774063/3211029
 PARENT_PATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 CURRENT_PATH="${PWD}"
+cd "${PARENT_PATH}"
 
 # Eval '.env' file to read variables
-cd "${PARENT_PATH}"
-source ../.env
+ENV_PATH="$(realpath ${PARENT_PATH}/../.env)"
+echo "Sourcing env file: ${ENV_PATH}"
+source $ENV_PATH
 
 # Since redis cli will run inside docker, use alias to docker host
 if [ "${REDIS_HOST}" = "localhost" ]; then
-    REDIS_HOST=host.docker.internal
+    DOCKER_INTERNAL_LOCALHOST_REF=host.docker.internal
+    echo "Updating REDIS_HOST value to: ${DOCKER_INTERNAL_LOCALHOST_REF}"
+    REDIS_HOST=$DOCKER_INTERNAL_LOCALHOST_REF
 fi
 
+echo
+echo "Pushing notification to redis list:"
+echo "  > RPUSH ${QUEUE_NOTIFICATIONS} ..."
 docker run -it --rm             \
     redis:7.2.0-alpine          \
     redis-cli                   \
