049ef0b7bf4c48c0dd8349f308da2db411151cf0
Giovanni Aguirre <giovanni.fi05@gmail.com>
2024-03-12 23:25:26 -0600

    Show original input for verification

--- scripts/hashpw.py
+++ scripts/hashpw.py
@@ -4,6 +4,8 @@ import sys
 if len(sys.argv) < 2:
     raise Exception('Value to hash is required')
 
-value_to_hash = str(sys.argv[1]).encode()
-hashed_value = bcrypt.hashpw(value_to_hash, bcrypt.gensalt())
+raw_input = str(sys.argv[1])
+print(f'Input: { raw_input }')
+
+hashed_value = bcrypt.hashpw(raw_input.encode(), bcrypt.gensalt())
 print(hashed_value.decode())
