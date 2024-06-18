128a0e317dfa097a48be0cc30f5ddb582b91f59b
Giovanni Aguirre <giovanni.fi05@gmail.com>
2024-06-18 01:19:12 -0600

    Add support for RenderTemplateAction

--- central/observers/action_runners/__init__.py
+++ central/observers/action_runners/__init__.py
@@ -1,10 +1,12 @@
 import logging
 from ..models import (
     Action,
-    RedisListRPushAction
+    RedisListRPushAction,
+    RenderTemplateAction,
 )
 from .base import ARunner
 from .redis_list_rpush_runner import RedisListRPushRunner
+from .render_template_runner import RenderTemplateRunner
 
 
 logger = logging.getLogger(__name__)
@@ -13,5 +15,7 @@ logger = logging.getLogger(__name__)
 async def action_runner_from(action: Action) -> ARunner:
     if type(action) == RedisListRPushAction:
         return RedisListRPushRunner(action)
+    elif type(action) == RenderTemplateAction:
+        return RenderTemplateRunner(action)
     else:
         logger.warning(f'Action type {type(action)} not supported')
--- tests/unit/observers/action_runners/test_render_template_runner.py
+++ tests/unit/observers/action_runners/test_render_template_runner.py
@@ -1,6 +1,5 @@
 import os
 import pytest
-from unittest.mock import AsyncMock, patch
 from central.observers.action_runners.render_template_runner import (
     RenderTemplateRunner
 )
@@ -40,7 +39,37 @@ class TestRenderTemplateAction:
         )
         action_runner = RenderTemplateRunner(action)
 
-        await action_runner.run(context={})
+        await action_runner.run(context={'foo': 'bar'})
 
         with open(output_abs_path, 'r') as f:
             assert f.read() == '<h1>A template without variables</h1>'
+
+
+    @pytest.mark.asyncio
+    async def test_with_context_template(self):
+        t_rel_path = 'tests/templates/temperature.template'
+        t_abs_path = os.path.abspath(t_rel_path)
+        template_uri = 'file://{}'.format(t_abs_path)
+
+        output_rel_path = 'tests/templates/temperature.html'
+        output_abs_path = os.path.abspath(output_rel_path)
+        output_uri = 'file://{}'.format(output_abs_path)
+
+        action = RenderTemplateAction(
+            name='test_temperature',
+            template_uri=template_uri,
+            output_uri=output_uri
+        )
+        action_runner = RenderTemplateRunner(action)
+
+        await action_runner.run(context={
+            'name': 'John Doe',
+            'celsius_temp': 22
+        })
+
+        # Verify rendered content
+        with open(output_abs_path, 'r') as f:
+            assert f.read() == (
+                '<h1>Hello John Doe</h1>\n'
+                '<p>Current temperature is 22 celsius</p>'
+            )
